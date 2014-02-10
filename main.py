39b7cc73192305739a96b92e782fc67fe54f8516
Jocke <s@xyr.se>
2014-02-10 15:35:22 +0100

    2—1 @ bool vs string return-value in valid_settings()

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import common, logger
 import irc
 
 
-def validate_settings(settings, log=logger.log):
+def valid_settings(settings, log=logger.log):
     essentials = {'irc': {
                      'nick': str,
                      'channel': str,
@@ -19,15 +19,17 @@ def validate_settings(settings, log=logger.log):
                 setting_type = essentials[category][setting]
                 if type(settings[category][setting]) != setting_type:
                     log('error', 'Invalid settings: {}/{} must be of type {}. Closing.'.format(category, setting, str(setting_type)))
-                    return 'quit'
+                    return False
             except KeyError:
                 log('error', 'Invalid settings: {}/{} not found. Closing.'.format(category, setting))
-                return 'quit'
+                return False
+
+    return True
 
 
 def main():
     settings = common.read_config('config')
-    if validate_settings(settings) == 'quit':
+    if not valid_settings(settings):
         exit()
 
     while True:
--- test_main.py
+++ test_main.py
@@ -1,13 +1,13 @@
 import tests
 import main
 
-def test_validate_settings(logger):
+def test_valid_settings(logger):
     flog = tests.FakeLogger()
     
     logger.print('Testing validation of broken settings...')
 
-    result = main.validate_settings({}, log=flog.log)
-    assert result == 'quit'
+    result = main.valid_settings({}, log=flog.log)
+    assert not result
     assert flog.logged
 
     settings = {'irc': {
@@ -20,36 +20,36 @@ def test_validate_settings(logger):
                }}
 
     flog.logged = False
-    result = main.validate_settings(settings, log=flog.log)
+    result = main.valid_settings(settings, log=flog.log)
 
-    assert result == 'quit'
+    assert not result
     assert flog.logged
 
     logger.print('Testing with missing settings')
 
     settings['irc'].pop('nick')
     flog.logged = False
-    result = main.validate_settings(settings, log=flog.log)
+    result = main.valid_settings(settings, log=flog.log)
 
-    assert result == 'quit'
+    assert not result
     assert flog.logged
 
     logger.print('Testing with valid settings...')
 
     flog.logged = False
     settings['irc']['nick'] = 'nick'
-    result = main.validate_settings(settings, log=flog.log)
+    result = main.valid_settings(settings, log=flog.log)
 
-    assert result != 'quit'
+    assert result
     assert not flog.logged
 
     logger.print('Testing with additional settings...')
 
     flog.logged = False
     settings['irc']['lag'] = 'don\'t'
-    result = main.validate_settings(settings, log=flog.log)
+    result = main.valid_settings(settings, log=flog.log)
 
-    assert result != 'quit'
+    assert result
     assert not flog.logged
 
     return True
@@ -57,7 +57,7 @@ def test_validate_settings(logger):
 
 def test_run_all(logger):
     logger.print('Running all tests...')
-    assert test_validate_settings(logger.deeper('validate_settings'))
+    assert test_valid_settings(logger.deeper('valid_settings'))
     logger.print('All tests complete!')
     return True
 
